67495b1fe37484916b75ef87ffe3a9ba6a9457e5
Chen Wilson <chenlwilson@gmail.com>
2018-11-27 12:02:06 -0800

    cleaned up parseJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,20 +6,35 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   // your code here
+  // var result = [];
+  // var searchNode = function(node) {
+  //   //check parent node
+  //   if (node.classList !== undefined && node.classList.contains(className)) {
+  //     result.push(node);
+  //   }
+  //   //then check for child nodes
+  //   if (node.childNodes !== undefined && node.childNodes.length > 0) {
+  //     for (var i = 0; i < node.childNodes.length; i++) { //iterate through the node.classList
+  //       searchNode(node.childNodes[i]); //start recursion on child nodes
+  //     }
+  //   }
+  // };
+  //
+  // searchNode(document.body);
+  // return result;
   var result = [];
-  var searchNode = function(node) {
-    //check parent node
-    if (node.classList !== undefined && node.classList.contains(className)) {
+  var recurse = function(node) {
+    if (node.classList && node.classList.contains(className)) {
       result.push(node);
     }
-    //then check for child nodes
-    if (node.childNodes !== undefined && node.childNodes.length > 0) {
-      for (var i = 0; i < node.childNodes.length; i++) { //iterate through the node.classList
-        searchNode(node.childNodes[i]); //start recursion on child nodes
-      }
+    if (node.children.length > 0) {
+      [...node.children].forEach(function(child) {
+        recurse(child);
+      })
     }
-  };
+  }
 
-  searchNode(document.body);
+  recurse(document.body);
+
   return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,44 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-  //function, undefined
-
-
-  if (typeof obj === "function" || typeof obj === undefined || obj === null) {
-    return 'null';
-  }
-  if (typeof obj === "boolean" || typeof obj === "number") {
-    return obj.toString();
-  }
-  if (typeof obj === "string") { //string
+  //primitive
+  if (typeof obj === 'string') {
     return '"' + obj + '"';
   }
-
-
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (obj === null || obj === NaN || obj === Infinity) {
+    return 'null';
+  }
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+    return null;
+  }
+  //array & object
   if (Array.isArray(obj)) {
-    var strArr = '[';
-    for (var i = 0; i < obj.length; i++) {
-      strArr += stringifyJSON(obj[i]) + ',';
-    }
-
-    if (strArr.length > 1) {
-      return strArr = strArr.slice(0, strArr.length - 1) + ']';
-    } else {
-      return strArr + ']';
-    }
-  }
-  //obj
-  var strObj = '{';
-  for (var key in obj) {
-    if (obj[key] !== undefined && typeof obj[key] !== "function") {
-      strObj += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
-    }
-  }
-  if (strObj.length > 1) {
-    return strObj = strObj.slice(0, strObj.length - 1) + '}';
+    var mappedArr = obj.map(function(item) {
+      return stringifyJSON(item);
+    })
+    return '[' + mappedArr.join() + ']';
   } else {
-    return strObj + '}';
+    var mappedObj = [];
+    for (var key in obj) {
+      if (stringifyJSON(obj[key]) !== null) {
+        mappedObj.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + mappedObj.join() + '}';
   }
-
 };
